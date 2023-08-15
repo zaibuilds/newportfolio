16a6243010fd39445dc5b16528a168bbb30bfa9c
Zai Builds <hello@zaibuilds.io>
2023-08-15 17:34:31 +0100

    typing and deleting one word ✅

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
+
+const dynamicText = document.querySelector("h1 span");
+
 const words = ["Product Designer", "Creative Technologist", "Software Engineer"];
 
 let wordIndex = 0;
-let charIndex = 0;
+let charIndex = 1;
+let isDeleting = false;
 
 const typeEffect = () => {
  const currentWord = words[wordIndex];
  const currentChar = currentWord.substring(0, charIndex);
- console.log(currentWord, currentChar);
+ dynamicText.textContent = currentChar;
+
+ if(!isDeleting && charIndex < currentWord.length) {
+    // If condition is true type the next character
+    charIndex++;
+    setTimeout(typeEffect, 200 );
+ } else if (isDeleting && charIndex > 0) {
+    // If condition is false remove the previous character
+    charIndex--;
+    setTimeout(typeEffect, 100 );
+
+ } else {
+    isDeleting = !isDeleting;
+    setTimeout(typeEffect, 1200);
+ }
 }
 
 typeEffect();
